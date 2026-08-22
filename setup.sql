ee4a1ef1d3553ef7f197527ff31a00354f198229
maliknihad004 <maliknihad04@gmail.com>
2026-08-22 15:29:13 +0300

    Remove database credentials from setup

--- setup.sql
+++ setup.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE pricepulse;
 
 -- Create a dedicated PostgreSQL user for the PricePulse application
-CREATE USER pricepulse WITH PASSWORD 'pricepulse';
+CREATE USER pricepulse;
 
 -- Give the PricePulse user access to the PricePulse database
 GRANT ALL PRIVILEGES ON DATABASE pricepulse TO pricepulse;
