4faa5b3babc5d140e66c5639420ba837b17f48e3
maliknihad004 <maliknihad04@gmail.com>
2026-08-31 10:28:41 +0300

    Remove unused scripts

--- pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-read -p "Branch to pull: " branch
-
-git pull origin "$branch"
--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Branch to push: " branch
-read -p "Commit message: " msg
-
-git add .
-git commit -m "$msg"
-git push origin "$branch"
